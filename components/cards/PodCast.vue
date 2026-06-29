19bfbcb58f94bca938593102f1d06222211e5142
Pipul Boddyo <pipul.boddyo@gmail.com>
2026-06-29 14:44:38 +0600

    deploy

--- components/cards/PodCast.vue
+++ components/cards/PodCast.vue
@@ -2,7 +2,7 @@
     <div
         class="lg:p-1 relative h-auto flex flex-col lg:rounded-2xl lg:bg-box-bg lg:shadow-lg lg:shadow-box-shadow lg:border lg:border-box-border">
         <div class="relative h-max min-h-max">
-            <nuxt-img :src="resolvedCoverImage" alt="Cover image" width="280"
+            <img :src="resolvedCoverImage" alt="Cover image" width="280"
                 class="w-full h-32 sm:h-36 md:h-40 lg:h-48 rounded-xl object-cover"/>
                 <span class="absolute top-2 right-2 px-2 rounded-full text-sm text-white bg-primary">
             {{ duration }}
